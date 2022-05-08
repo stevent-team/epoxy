b8a368fc871f22054c03d3e4334c167a260ce4ed
Ewan Breakey <ebreakey@gmail.com>
2022-05-08 17:00:40 +1000

    Implement generic system for meta tag manipulation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, Responder, App, HttpServer, HttpRequest, Result, HttpResponse};
+use actix_web::{web, Responder, App, HttpServer, HttpRequest, HttpResponse};
 use actix_web::dev::{ServiceRequest, ServiceResponse, fn_service};
 use actix_files::NamedFile;
 use actix_files as fs;
@@ -10,37 +10,34 @@ async fn event(req: HttpRequest) -> impl Responder {
     let path: PathBuf = req.match_info().query("eventid").parse().unwrap();
     println!("Get event: {:?}", path);
     
-    // Pull details
-    let details = meta::Details {
+    // Create event details
+    let details = meta::EventDetails {
         title: String::from("Test Event"),
         description: String::from("A cool event you should go to"),
         image_url: String::from("https://storage.googleapis.com/stevent-development-image-bucket/e6d3a632-d10e-49ab-bad5-b558ba58b163"),
         start_time: String::from("2022-05-08T06:40:52"),
         end_time: String::from("2022-05-08T06:40:52"),
     };
-    
-    // Create meta tags
-    let meta_tags = meta::tags_from_details(details);
-    let meta_content = meta_tags.iter()
-        .map(|tag| tag.to_string())
-        .fold(String::new(), |a, b| a + &b + "\n");
-
-    // Read index html file
-    let index_contents = std::fs::read_to_string("./static/index.html").expect("Can't find index.html");
-
-    // Insert meta tags
-    let head_split : Vec<&str> = index_contents.split("</head>").collect();
-    let augmented_contents : String = head_split[0].to_owned() + &meta_content +  "</head>" +  head_split[1];
 
     HttpResponse::Ok()
         .content_type("text/html")
-        .body(augmented_contents)
+        .body(meta::index_with_meta_tags(meta::tags_from_event_details(details)))
 }
 
-async fn club(req: HttpRequest) -> Result<NamedFile> {
+async fn club(req: HttpRequest) -> impl Responder {
     let path: PathBuf = req.match_info().query("clubid").parse().unwrap();
     println!("Get club: {:?}", path);
-    Ok(NamedFile::open("./static/index.html")?)
+    
+    // Create event details
+    let details = meta::ClubDetails {
+        title: String::from("Test Event"),
+        description: String::from("A cool event you should go to"),
+        image_url: String::from("https://storage.googleapis.com/stevent-development-image-bucket/e6d3a632-d10e-49ab-bad5-b558ba58b163"),
+    };
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(meta::index_with_meta_tags(meta::tags_from_club_details(details)))
 }
 
 #[actix_web::main]
--- src/meta.rs
+++ src/meta.rs
@@ -1,11 +1,3 @@
-pub struct Details {
-  pub title: String,
-  pub description: String,
-  pub image_url: String,
-  pub start_time: String,
-  pub end_time: String,
-}
-
 pub struct Tag<'a> {
     property: &'a str,
     content: String,
@@ -17,7 +9,21 @@ impl Tag<'_> {
     }
 }
 
-pub fn tags_from_details(details: Details) -> Vec<Tag<'static>> {
+pub struct EventDetails {
+  pub title: String,
+  pub description: String,
+  pub image_url: String,
+  pub start_time: String,
+  pub end_time: String,
+}
+
+pub struct ClubDetails {
+  pub title: String,
+  pub description: String,
+  pub image_url: String,
+}
+
+pub fn tags_from_event_details(details: EventDetails) -> Vec<Tag<'static>> {
   vec![
     Tag { property: "og:title", content: details.title },
     Tag { property: "og:description", content: details.description },
@@ -29,4 +35,31 @@ pub fn tags_from_details(details: Details) -> Vec<Tag<'static>> {
     Tag { property: "twitter:card", content: String::from("summary_large_image") },
     Tag { property: "theme-color", content: String::from("#b99bff") },
   ]
+}
+
+pub fn tags_from_club_details(details: ClubDetails) -> Vec<Tag<'static>> {
+  vec![
+    Tag { property: "og:title", content: details.title },
+    Tag { property: "og:description", content: details.description },
+    Tag { property: "og:image", content: details.image_url },
+    Tag { property: "og:site_name", content: String::from("Stevent") },
+    Tag { property: "twitter:card", content: String::from("summary_large_image") },
+    Tag { property: "theme-color", content: String::from("#b99bff") },
+  ]
+}
+
+pub fn index_with_meta_tags(tags: Vec<Tag<'static>>) -> String {
+    // Create meta tags
+    let meta_content = tags.iter()
+        .map(|tag| tag.to_string())
+        .fold(String::new(), |a, b| a + &b + "\n");
+
+    // Read index html file
+    let index_contents = std::fs::read_to_string("./static/index.html").expect("Can't find index.html");
+
+    // Insert meta tags
+    let head_split : Vec<&str> = index_contents.split("</head>").collect();
+    let augmented_contents : String = head_split[0].to_owned() + &meta_content +  "</head>" +  head_split[1];
+
+    augmented_contents
 }
