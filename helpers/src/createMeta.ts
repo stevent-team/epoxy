8da6f923306547a95d9949410a86ca8709ac7696
Ben Grant <benjamin.grantGRA0007@gmail.com>
2022-05-18 13:39:13 +1000

    Cut out null meta fields

--- helpers/src/createMeta.ts
+++ helpers/src/createMeta.ts
@@ -5,7 +5,7 @@ type MetaTags = {
 const createMeta = (metaTags: MetaTags): string =>
   Object.entries(metaTags)
   .reduce((html, [property, content]) =>
-    `${html}<meta property="${property}" content="${content}">`,
+    (property && content) ? `${html}<meta property="${property}" content="${content}">` : html,
     ''
   )
 
