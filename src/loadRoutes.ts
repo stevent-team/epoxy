fecba4b933d9a42f5ce7ad1ae0103a4713c81b90
Ben Grant <benjamin.grantGRA0007@gmail.com>
2022-05-18 11:36:08 +1000

    Load module from module

--- src/loadRoutes.ts
+++ src/loadRoutes.ts
@@ -1,7 +1,7 @@
 // Dynamically import the routes file
 const loadRoutes = async (routeFile: string) => {
   const module = await import(routeFile)
-  return module?.default
+  return module?.default?.default ?? module?.default
 }
 
 export default loadRoutes
