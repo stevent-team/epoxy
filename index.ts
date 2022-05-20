284fe241bc100098609439960357d271b932ea42
Ben Grant <benjamin.grantGRA0007@gmail.com>
2022-05-20 16:53:06 +1000

    Add build command that builds epoxy routes separately, and a flag

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 import yargs from 'yargs'
 import path from 'path'
+import { Parcel } from '@parcel/core'
 
 import loadRoutes from './src/loadRoutes'
 import startServer, { Routes } from './src/server'
 
-const serve = async ({ target, routeFile, host, port, index }) => {
+const serve = async ({ target, routeFile, host, port, index, build }) => {
   // Load routes
   let routes: Routes = {}
   if (routeFile) {
     const resolvedRoutes = path.resolve(routeFile)
-    routes = await loadRoutes(resolvedRoutes).catch(e => {
+    routes = await loadRoutes(resolvedRoutes, build).catch(e => {
       throw new Error(`Failed to load routes file "${resolvedRoutes}": ${e}`)
     })
   }
@@ -32,6 +33,31 @@ const serve = async ({ target, routeFile, host, port, index }) => {
   })
 }
 
+const build = async ({ routeFile, outputDir }) => {
+  // Setup bundler
+  const bundler = new Parcel({
+    entries: routeFile,
+    defaultConfig: '@parcel/config-default',
+    mode: 'production',
+    shouldDisableCache: true,
+    targets: {
+      'main': {
+        context: 'node',
+        distDir: outputDir,
+        engines: {
+          node: ">=10"
+        },
+        scopeHoist: false,
+        optimize: false,
+      }
+    }
+  })
+
+  // Build routes configuration
+  const { buildTime } = await bundler.run()
+  console.log(`✨ Built routes file in ${buildTime}ms`)
+}
+
 // Use yargs to parse command line options
 yargs
   .scriptName("epoxy")
@@ -41,25 +67,44 @@ yargs
     'Serve the provided static folder',
     yargs => yargs
       .positional('target', { describe: 'Path to static directory', require: true })
-      .positional('routeFile', { describe: 'Path to router script' }),
+      .positional('routeFile', { describe: 'Path to cjs router script (can use ES6 with --build option)' })
+      .option('port', {
+        alias: 'p',
+        type: 'string',
+        description: 'port to use for http server',
+        default: 8080,
+      })
+      .option('host', {
+        alias: 'h',
+        type: 'string',
+        description: 'host to use for http server',
+        default: '0.0.0.0',
+      })
+      .option('index', {
+        alias: 'i',
+        type: 'string',
+        description: 'path to index html inside of target',
+        default: 'index.html',
+      })
+      .option('build', {
+        alias: 'b',
+        type: 'boolean',
+        description: 'build routes file in memory',
+        default: false,
+      }),
     serve
   )
-  .option('port', {
-    alias: 'p',
-    type: 'string',
-    description: 'port to use for http server',
-    default: 8080,
-  })
-  .option('host', {
-    alias: 'h',
-    type: 'string',
-    description: 'host to use for http server',
-    default: '0.0.0.0',
-  })
-  .option('index', {
-    alias: 'i',
-    type: 'string',
-    description: 'path to index html inside of target',
-    default: 'index.html',
-  })
+  .command(
+    'build <routeFile>',
+    'Build a routes file',
+    yargs => yargs
+      .positional('routeFile', { describe: 'Path to ES6 router script', require: true })
+      .option('outputDir', {
+        alias: 'o',
+        type: 'string',
+        description: 'folder to output built routes file',
+        default: 'dist',
+      }),
+    build
+  )
   .argv
