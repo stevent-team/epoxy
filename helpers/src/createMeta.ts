66ffab1ed3aaef030ac8e009ed4bbb4f0f6bad67
Ben Grant <benjamin.grantGRA0007@gmail.com>
2022-06-29 11:18:46 +1000

    Escape HTML special chars from content of meta tags

--- helpers/src/createMeta.ts
+++ helpers/src/createMeta.ts
@@ -3,10 +3,17 @@ type MetaTags = {
 }
 
 const createMeta = (metaTags: MetaTags): string =>
-  Object.entries(metaTags)
-  .reduce((html, [property, content]) =>
-    (property && content) ? `${html}<meta property="${property}" content="${content}">` : html,
-    ''
-  )
+  Object.entries(metaTags).reduce((html, [property, content]) => {
+    if (!(property && content)) return html // Skip if unset
+
+    // Escape html entities
+    const escapedContent = content
+      .replace(/"/g, "&quot;")
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+
+    return `${html}<meta property="${property}" content="${escapedContent}">`
+  }, '')
 
 export default createMeta
